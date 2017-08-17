5c305e06992049fd122c8c31aaf3abd55d5e0437
kmarventano <krm411@nyu.edu>
2017-08-17 11:35:58 -0400

    adding the code

---
+++ app.js
@@ -0,0 +1,56 @@
+/**
+ * Created by Kristen on 8/17/17.
+ */
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const hbs = require("hbs");
+
+app.set('views', __dirname);
+app.set('view engine', 'hbs');
+
+require('./db');
+
+const Post = mongoose.model("Post");
+
+app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/finalproject.html');
+
+});
+
+app.get('/projectCode.html', function(req, res) {
+    res.sendFile(__dirname + '/projectCode.html')
+});
+
+app.get('/blog', function(req, res) {
+    Post.find({}, function(err, posts) {
+        res.render('blog', {posts: posts});
+    });
+});
+
+app.post('/blog', function(req, res) {
+    // You can modify the date to be the format you want
+    const date = (new Date()).toString();
+
+    const p = new Post({
+        name: req.body.name,
+        location: req.body.location,
+        entry: req.body.entry,
+        date: date
+    });
+
+    p.save(function (err) {
+        if(err) {
+            console.log(err);
+        }
+
+        res.redirect('/blog');
+    });
+});
+
+app.listen(3000);
